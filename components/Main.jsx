e2c09e01ad4d05482b238d4a9ed0c3564a5cb286
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-26 16:33:21 -0400

    created components in main to navlinks for about portfolio and home

---
+++ components/Main.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import Navbar from "./Navbar";
+import Portfolio from "./Portfolio";
+import About from "./About";
+
+<div id="main">
+<Navbar/>
+<Portfolio/>
+<About/>
+</div>
+
+export default Main
---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+
+<div>
+<NavLink id="portfolioLink" to="portfolio"> Portfolio </NavLink>
+        
+<NavLink id="aboutLink" to="about"> About </NavLink>
+
+<NavLink id="homeLink" to="home"> Home </NavLink>
+
+        </div>
+
+
+export default Navbar
